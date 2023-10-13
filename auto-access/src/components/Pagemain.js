f51fac180c844fcfa10968aa6089539a306fd0c0
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2023-10-13 16:15:24 +0600

    main page done

--- auto-access/src/components/Pagemain.js
+++ auto-access/src/components/Pagemain.js
@@ -1,11 +1,36 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState, useEffect } from "react";
+import Header from "./Header";  
+import './Pagemain.css'
 
 export default function Pagemain(){
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const images = [
+        "/images/car2.png",
+        "/images/car1.png",
+        "/images/car3.png",
+        "/images/car4.webp",
+    ];
+
+    const nextImage = () => {
+        setCurrentImage((currentImage + 1) % images.length);
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(nextImage, 3000);
+        return () => clearInterval(intervalId);
+    }, [nextImage]);
+
     return(
         <pagemain>
             <Header />
-            <img src="./public/images/car2.png" alt="Car"></img>
+            <div class = "explanationOfCompany">
+                <h1>Driving the Path to Happiness</h1>
+                <p>Search, Choose, Drive</p>
+            </div>
+            <div class = "image-container">
+                <img src={images[currentImage]} alt={`Image ${currentImage}`} />
+            </div>
         </pagemain>
     )
 }
