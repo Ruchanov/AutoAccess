2bd20d588f0e49f060137ac85fe59c5661b51dcb
Aybergen <ai_bolat@kbtu.kz>
2023-10-21 15:58:51 +0600

    Aiba

--- auto-access/src/components/main/Pagemain.js
+++ auto-access/src/components/main/Pagemain.js
@@ -45,49 +45,33 @@ export default function Pagemain() {
                 <p>Search, Choose, Drive</p>
             </div>
             <div className="image-container">
-                <img src={images[currentImage]} alt={`Image ${currentImage}`} />
+                <img src={images[currentImage]} alt={Image ${currentImage}} />
             </div>
         </div>
         <footer style={{ display: isFooterVisible ? "block" : "none" }}>
-            <div className="last">
-            <ol>
-                <li>О компании «Auto Access»</li>
-                <li>
-                <a href="#">Написать в Службу заботы</a>
-                </li>
-                <li>
-                  <a href="#">Работа в «Auto Access»</a>
-                </li>
-                <li>
-                  <a href="#">Карта сайта</a>
-                </li>
-                <li>
-                  <a href="#">Рекламодателям</a>
-                </li>
-                <li>
-                  <a href="#">Правила размещения объявлений</a>
-                </li>
-                <li>
-                  <a href="#">Пользовательское соглашение</a>
-                </li>
-                <li>
-                  <a href="#">Скачать приложение</a>
-                </li>
-                <li>
-                  <a href="#">Мобильная версия сайта</a>
-                </li>
-                <li>
-                  <a href="#">Следите за нашими новостями</a>
-                </li>
-                <li>
-                  <a href="#">
-                    Instagram YouTube Telegram TikTok VK Facebook
-                  </a>
-                </li>
-            </ol>
-        </div>
-      </footer>
+            <div className="footer-row">
+                <ul>
+                    <li>О компании «Auto Access»</li>
+                    <li><a href="https://wa.me/7776887839">Написать в Службу заботы</a></li>
+                    <li><a href="#">Работа в «Auto Access»</a></li>
+                </ul>
+            </div>
+            <div className="footer-row">
+                <ul>
+                    <li><a href="#">Карта сайта</a></li>
+                    <li><a href="#">Рекламодателям</a></li>
+                    <li><a href="#">Правила размещения объявлений</a></li>
+                </ul>
+            </div>
+            <div className="footer-row">
+                <ul>
+                    <li><a href="#">Пользовательское соглашение</a></li>
+                    <li><a href="#">Скачать приложение</a></li>
+                    <li><a href="#">Мобильная версия сайта</a></li>
+                </ul>
+            </div>
+        </footer>
+
     </div>
   );
 }
-
